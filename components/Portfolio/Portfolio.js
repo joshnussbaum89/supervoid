3bf8fed5057deda88078306308609d57dc3db90d
Josh Nussbaum <joshnussbaum89@gmail.com>
2022-08-26 01:01:11 -0500

    test data setup

--- components/Portfolio/Portfolio.js
+++ components/Portfolio/Portfolio.js
@@ -27,37 +27,38 @@ export default function Portfolio({ projectData, projectDataError }) {
   return (
     <section id="portfolio" className={portfolio}>
       <h2>portfolio</h2>
-      {projectDataError ? (
+      {/* {projectDataError ? (
         <h3>Failed to load portfolio, please reload the page and try again</h3>
       ) : !projectData ? (
         <h3>Loading...</h3>
-      ) : (
-        <div className={projects}>
-          {JSON.parse(projectData).map((project) => (
-            <div
-              className={projectContainer}
-              key={project.id}
-              data-gif={project.gif}
-              data-client={project.client}
-              data-project={project.project}
-              data-description={project.description}
-              onClick={handleModalDisplay}
-            >
-              <Image
-                className={image}
-                src={project.gif}
-                alt={`${project.client} - ${project.description}`}
-                width="100%"
-                height="100%"
-                layout="responsive"
-              />
-              <div className={projectOverlay}>
-                <h3>{project.client}</h3>
-              </div>
+      ) : ( */}
+      <div className={projects}>
+        {projectData.map((project) => (
+          <div
+            className={projectContainer}
+            key={project.id}
+            data-gif={project.gif}
+            data-client={project.client}
+            data-project={project.project}
+            data-description={project.description}
+            onClick={handleModalDisplay}
+          >
+            <Image
+              className={image}
+              src={project.gif}
+              alt={`${project.client} - ${project.description}`}
+              width="100%"
+              height="100%"
+              layout="responsive"
+            />
+            <div className={projectOverlay}>
+              <h3>{project.client}</h3>
             </div>
-          ))}
-        </div>
-      )}
+          </div>
+        ))}
+      </div>
+      {/* ) */}
+      {/* } */}
       <ProjectModal
         modalIsActive={modalIsActive}
         modalData={modalData}
