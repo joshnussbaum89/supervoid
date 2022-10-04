365db85d1d1fada5e8da47c7f38882a1846a0122
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2022-10-04 09:16:04 -0500

    update dependencies, fix Vimeo script warning

--- pages/_document.js
+++ pages/_document.js
@@ -1,32 +1,31 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document'
 import Script from 'next/script'
 
-export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+KR:wght@100;200;300;400;500;600;700&display=swap"
-            rel="stylesheet"
-          />
-          <link rel="icon" href="/images/supervoid-favicon.png" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-          <Script
-            src="https://player.vimeo.com/api/player.js"
-            strategy="beforeInteractive"
-          />
-        </body>
-      </Html>
-    )
-  }
+// TODO: put code here that will be used on all pages
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+KR:wght@100;200;300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="icon" href="/images/supervoid-favicon.png" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+        <Script
+          src="https://player.vimeo.com/api/player.js"
+          strategy="lazyOnload"
+        />
+      </body>
+    </Html>
+  )
 }
