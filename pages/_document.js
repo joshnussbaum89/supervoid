f91704ab8e68da4cbeeb74ddbb7120bfe0b0edbc
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2023-03-14 14:38:07 -0500

    update favicon

--- pages/_document.js
+++ pages/_document.js
@@ -5,10 +5,26 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link
-          rel="icon"
-          href="/images/supernova-site-background.webp"
-          type="image/png"
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
         />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000100" />
+        <meta name="msapplication-TileColor" content="#000100" />
+        <meta name="theme-color" content="#000100" />
       </Head>
       <body>
         <Main />
