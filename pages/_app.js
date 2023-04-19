cf291c07c14dc284074a5c2bc5bb11b135bc7ea0
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2023-04-19 11:11:28 -0500

    optimize fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 // Components, hooks
 import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
+import localFont from 'next/font/local'
 import Layout from '../components/Global/Layout/Layout'
 
 // Styles
 import '../styles/globals.css'
+
+// Fonts
+const benzin_regular = localFont({ src: '../public/fonts/benzin_regular.ttf' })
+const benzin_bold = localFont({ src: '../public/fonts/benzin_bold.ttf' })
 
 function MyApp({ Component, pageProps }) {
   const [mobileNavOverlayDisplayed, setMobileNavOverlayDisplayed] =
@@ -20,13 +25,31 @@ function MyApp({ Component, pageProps }) {
         body.classList.remove('mobile-nav-overlay-active')
       }
     }
-  })
+  }, [mobileNavOverlayDisplayed])
 
   // Track URL path
   const urlPath = useRouter().pathname.replace('/', '')
 
   return (
     <>
+      <style jsx global>{`
+        :root {
+          --font-supervoid-body: ${benzin_regular.style.fontFamily};
+          --font-supervoid-header: ${benzin_bold.style.fontFamily};
+        }
+
+        body {
+          font-family: var(--font-supervoid-body);
+        }
+
+        h1,
+        h2,
+        h3,
+        h4,
+        h5 {
+          font-family: var(--font-supervoid-header);
+        }
+      `}</style>
       <Layout
         mobileNavOverlayDisplayed={mobileNavOverlayDisplayed}
         setMobileNavOverlayDisplayed={setMobileNavOverlayDisplayed}
