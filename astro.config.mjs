b9cdf9ccbde9e04209b7d99677f5f39a2957d029
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2024-04-02 16:16:55 -0500

    install packages, basic layout, SEO, styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,18 @@
-import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap'
+import icon from 'astro-icon'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://www.supervoid.tv',
+  scopedStyleStrategy: 'class',
+  build: {
+    inlineStylesheets: 'always',
+  },
+  integrations: [
+    icon(),
+    sitemap({
+      lastmod: new Date(),
+    }),
+  ],
+})
