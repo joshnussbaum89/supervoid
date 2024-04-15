73226c6730080af11ed379bc665f5d6a1198211e
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2024-04-15 12:42:48 -0500

    add Sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,28 @@
 import netlify from '@astrojs/netlify'
 import sitemap from '@astrojs/sitemap'
+import sentry from '@sentry/astro'
 import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.supervoid.tv',
+  output: 'hybrid',
   scopedStyleStrategy: 'class',
+  adapter: netlify({
+    imageCDN: false,
+    cacheOnDemandPages: true,
+  }),
   integrations: [
     icon(),
     sitemap({
       lastmod: new Date(),
     }),
+    sentry({
+      dsn: 'https://7067e3e9ec200c9fc02667a9bc86d558@o4505993173467136.ingest.sentry.io/4505993189588992',
+      sourceMapsUploadOptions: {
+        project: 'supervoid',
+      },
+    }),
   ],
-  output: 'hybrid',
-  adapter: netlify({
-    imageCDN: false,
-    cacheOnDemandPages: true,
-  }),
 })
